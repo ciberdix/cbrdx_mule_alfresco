8b7cba8692d86edfa681d7f1fb7750dad310d6f0
vladeye <vladimir.rodriguez@ciberdix.com>
2017-07-24 07:32:13 -0500

    Proyecto para mule

---
+++ src/main/java/com/crezcamos/mule/alfresco/cmis/content/GetContentProps.java
@@ -0,0 +1,30 @@
+package com.crezcamos.mule.alfresco.cmis.content;
+
+import org.apache.chemistry.opencmis.client.api.Document;
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.lifecycle.Callable;
+
+import com.crezcamos.mule.alfresco.cmis.content.ContentPropsBean;
+
+
+public class GetContentProps implements Callable
+{
+
+	@Override
+	public Object onCall(final MuleEventContext eventContext) throws Exception
+	{
+
+		final MuleMessage incomingMessage = eventContext.getMessage();
+
+		final Document cmisAlfrescoDocObject = (Document) incomingMessage.getPayload();// get cmisObject
+
+		final ContentPropsBean contentPropObject = new ContentPropsBean();
+
+		contentPropObject.setTitle(cmisAlfrescoDocObject.getProperty("cm:title").getValueAsString());// set title
+		contentPropObject.setName(cmisAlfrescoDocObject.getName()); // set name
+		contentPropObject.setDescription(cmisAlfrescoDocObject.getProperty("cm:description").getValueAsString());// set description
+		contentPropObject.setCreator(cmisAlfrescoDocObject.getProperty("cmis:createdBy").getValueAsString());// set created by
+		return contentPropObject;
+	}
+}
---
+++ src/main/java/com/crezcamos/mule/alfresco/cmis/content/PostContentProps.java
@@ -0,0 +1,14 @@
+package com.crezcamos.mule.alfresco.cmis.content;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+
+public class PostContentProps implements Callable {
+
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
